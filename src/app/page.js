6488314dff34497bd57cfa215c56b871f25d2d4e
Aurawell__ <viniciusavila4080@gmail.com>
2025-02-24 09:50:33 -0400

    inicial

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,32 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
+import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+const LoginPage = () => {
   return (
-    <main>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.h1} >CON<span className={styles.span}>CORD</span></h1>
+      <Image className={styles.img} alt="img" src="/images/Rectangle 15.png" width={140} height={140} />
+      </header>
       
-    </main>
+      <main className={styles.main_content}> 
+        <h1  className={styles.title}>Bem-vindo</h1>
+        <p className={styles.description}>
+          Aqui você encontrará um ambiente acolhedor e seguro para compartilhar ideias e se conectar com outras pessoas.
+        </p>
+        <button className={styles.button}>
+          <Link href="./login">Login</Link>
+        </button>
+
+        <p className={styles.link}>
+          Ainda não tem conta? <Link href="./cadastrar">Cadastrar-se</Link>
+        </p>
+      </main>
+    </div>
   );
-}
+};
+
+export default LoginPage;
