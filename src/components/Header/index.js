a2d81ea09a9f0eebafe75d9af99f37d4216f295c
clara1309 <rodriguesnunesa7@gmail.com>
2025-04-02 15:45:30 -0400

    aaa

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import Image from "next/image"
 export default function Header() {
+    
     const [menuOpen, setMenuOpen] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
 
@@ -23,27 +24,23 @@ export default function Header() {
 
     return (
         <header className={`${styles.header} ${menuOpen ? styles.menuOpen : ''}`}>
-            <div className={styles.logo}>
+            <div >
 
                 {<Image className={styles.img} src='/images/logo.png' alt="Concord" width={100} height={50}/>}
                 
             </div>
-
+            <div className={styles.dConcord}>
+                    <h1>Con<span className={styles.span}>cord</span></h1>
+                    <p className={styles.paragráfo}>Chat online</p>
+                </div>
             <div 
             className={styles.menuIcon} 
               onClick={menuOpen ? closeMenu : openMenu}>
                   {menuOpen ? '✖' : '☰'}
             </div>
 
-            <ul className={`${styles.lista} ${menuOpen ? styles.open : ''}`}>
-                <li>
-                    <Link href="/" className={`${styles.Link}`}>Home</Link>
-                    
-                </li>
 
-
-
-            </ul>
+        
         </header>
     );
 }
