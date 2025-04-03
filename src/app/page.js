53f853e737791ea99b79fc4321380b6b97d4ef5b
Aurawell__ <viniciusavila4080@gmail.com>
2025-04-03 09:39:03 -0400

    update

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ import Cookies from "js-cookie";
 
 const LoginPage = () => {
 
-  Cookies.remove('key');
-  Cookies.remove('newAccount');
-  console.log(Cookies.get());
+  //Cookies.remove('key');
+  //Cookies.remove('newAccount');
+  //console.log(Cookies.get());
 
   return (
     <div className={styles.container}>
@@ -18,19 +18,6 @@ const LoginPage = () => {
       <main className={styles.main_content}> 
         <h1  className={styles.title}>Bem-vindo ao Concord</h1>
         <p className={styles.description}>
-<<<<<<< HEAD
-        Estamos felizes por ter você aqui! O Concord é um
-         espaço criado  para conectar
-          pessoas,</p>
-       <p className={styles.description_para}> e 
-          tornar a comunicação mais dinâmica e interativa.
-          Você encontrará um ambiente acolhedor.</p>
-    
-        <div className="image-container">
-        <Image className={styles.img} alt="img" src="/images/i4.jpg" width={700} height={700} />
-      </div>
-  
-=======
           Onde as Conversas Ganham Vida! Aqui, cada mensagem é uma nova conexão. 
         </p>
         <p className={styles.description}>
@@ -43,7 +30,6 @@ const LoginPage = () => {
           <Link href="./login">Login</Link>
         </button>
 
->>>>>>> 10ad8471c8662b3a349f4d838cd9c73e8471b8cd
         <p className={styles.link}>
           Ainda não tem conta? <Link className={styles.link2} href="./cadastrar">Cadastre-se</Link>
         </p>
