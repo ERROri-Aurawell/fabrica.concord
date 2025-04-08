3876257dfcf7ac67fb9b7d6568be3f32f0838dde
clara1309 <rodriguesnunesa7@gmail.com>
2025-04-08 16:44:56 -0400

    a22

--- src/app/page.js
+++ src/app/page.js
@@ -18,18 +18,17 @@ const LoginPage = () => {
       <main className={styles.main_content}> 
         <h1  className={styles.title}>Bem-vindo ao Concord</h1>
         <p className={styles.description}>
-          Onde as Conversas Ganham Vida! Aqui, cada mensagem é uma nova conexão. 
-        </p>
-        <p className={styles.description}>
-        Faça novos amigos e descubra diferentes pontos de vista. 
-        </p>
-        <p className={styles.description}>
-        Seja para um bate-papo descontraído ou uma discussão mais profunda, este é o seu espaço para se expressar e se conectar.
-        </p>
-        <button className={styles.button}>
-          <Link href="./login">Login</Link>
-        </button>
-
+        Estamos felizes por ter você aqui! O Concord é um
+         espaço criado  para conectar
+          pessoas,</p>
+       <p className={styles.description_para}> e 
+          tornar a comunicação mais dinâmica e interativa.
+          Você encontrará um ambiente acolhedor.</p>
+    
+        <div className="image-container">
+        <Image className={styles.img} alt="img" src="/images/i4.jpg" width={700} height={700} />
+      </div>
+  
         <p className={styles.link}>
           Ainda não tem conta? <Link className={styles.link2} href="./cadastrar">Cadastre-se</Link>
         </p>
