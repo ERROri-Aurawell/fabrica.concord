36877c05fbf3baec62b2324635d26f6e0052c043
yasminmo2021 <santosmorenoyasmin1@gmail.com>
2025-07-09 15:53:17 -0400

    09.07 yasmin

--- src/app/configuracoes/page.js
+++ src/app/configuracoes/page.js
@@ -96,9 +96,19 @@ export default function Config() {
                             Atualizar Perfil
                         </button>
                     </div>
-                 
+                    <div className={styles.Gbotoes}>
 
-                
+                        <button onClick={() => mudardiv("2")} className={styles.botao}>
+                            Privacidade
+                        </button>
+                    </div>
+
+                    <div className={styles.Gbotoes}>
+
+                        <button onClick={() => mudardiv("4")} className={styles.botao}>
+                            Notificações
+                        </button>
+                    </div>
                     <div className={styles.Gbotoes}>
 
                         <button onClick={() => mudardiv('5')} className={styles.botao}>
@@ -108,96 +118,186 @@ export default function Config() {
                 </section>
 
                 <section className={styles.parteSumivel}>
-
-
-                    {div1 && (
-                        <div className={styles.divSu}>
-                            <div className={styles.miniperfil}>
-                                <img width={90} height={90} alt="perfil" src={selectedAvatar}>
-                                    
-                                </img>
-                                <p className={styles.nome}>Nome</p>
-                            
-                                <section className={styles.mainCN}>
-                                    <div className={styles.dFundoCN}>
-                                        <section className={styles.sectionCN}>
-                                            <form className={styles.formCN} action={search}>
-
-                                                <h1 className={styles.h1CN}>Atualizar Perfil</h1>
-                                                <label className={styles.labelCN}>Nome:</label>
-                                                <input className={styles.input} type="text" id="nome" name="nome" required />
-                                                <p className={styles.noteCN}>Esse não é seu nome de usuário nem seu PIN. Esse nome será exibido para seus contatos Concord.</p>
-
-                                                <label className={styles.labelCN}>Descrição:</label>
-                                                <input className={styles.input} type="descrição" id="descrição" name="descrição" required />
-                                                <input  type="submit" className={styles.botaoCN} value="Atualizar seu perfil" />
-                                            </form>
-                                        </section>
-                                    </div>
-                                </section>
-                            </div>
-                        </div>
-                    )}
-
-
-
-                    {div3 && (
-                        <div className={styles.divSuMUD}>
-                            <div className={styles.containerMUD}>
-                                <div className={styles.headerMUD}>
-                                </div>
-
-                                <div className={styles.sectionMUD}>
-                                    <h3 className={styles.h3MUD}>Avatar atual</h3>
-                                    <div className={styles.currentMUD}>
-                                        <img src={selectedAvatar} alt="Avatar Atual" />
-                                    </div>
-                                </div>
-
-                                <div className={styles.sectionMUD}>
-                                    <h3 className={styles.h3MUD}>Alterar avatar</h3>
-                                    <div className={styles.gridMUD}>
-                                        {avatars.map((avatar, index) => (
-                                            <img
-                                                width={105}
-                                                key={index}
-                                                src={avatar}
-                                                alt={`avatar ${index + 1}`}
-                                                className={`option ${selectedAvatar === avatar ? "selected" : ""}`}
-                                                onClick={() => setSelectedAvatar(avatar)}
-                                            />
-                                        ))}
-
-                                    </div>
-                                </div>
-                                <button className={styles.buttonMUD}> Atualizar avatar</button>
-
-                            </div>
-                        </div>
-                    )}
-
                     
-                    {div5 && (
-                        <div className={styles.divSu}>
-                            <div className={styles.tabela_medicDL}>
-                                <h2 className={styles.h2DL}>Apagar conta</h2>
 
-                                <div className={styles.warningDL}>
-                                    <span className={styles.iconDL}>⚠</span>
-                                    <span className={styles.textDL}>Ao deletar essa conta:</span>
-                                </div>
-                                <p className={styles.pDL}>A conta será apagada do Concord e removida de todos os dispositivos.</p>
-                                <p className={styles.pDL}>Seu histórico de mensagens será apagado.</p>
-                                <p className={styles.pDL}>Você sairá de todos os grupos.</p>
-                                <button className={styles.deleteDL}>
-                                    <Link className={styles.pDL} href="./"> Apagar conta </Link>
-                                </button>
-                            </div>
+
+                {div1 && (
+                    <div className={styles.divSu}>
+                    <div className={styles.miniperfil}>
+                        <img width={90} height={90} alt="perfil" src={selectedAvatar} />
+                        <p className={styles.nome}>Nome</p>
+
+                        <section className={styles.mainCN}>
+                        <div className={styles.dFundoCN}>
+                            <section className={styles.sectionCN}>
+                            <form
+                                className={styles.formCN}
+                                onSubmit={async (e) => {
+                                e.preventDefault();
+
+                                const nome = e.target.nome.value;
+                                const desc = e.target.desc.value;
+                                const foto = getFotoNumber();
+
+                                try {
+                                    const response = await fetch(
+                                    "https://apiconcord.dev.vilhena.ifro.edu.br/updateUser/1-viniciusavila4080@gmail.com-7db940709d83364a8257d6f361b6e13d21622e40",
+                                    {
+                                        method: "PATCH",
+                                        headers: {
+                                        "Content-Type": "application/json",
+                                        },
+                                        body: JSON.stringify({ nome, foto, desc }),
+                                    }
+                                    );
+
+                                    if (response.ok) {
+                                    const data = await response.json();
+                                    console.log("Resposta da API:", data);
+                                    alert("Perfil atualizado com sucesso!");
+                                    } else {
+                                    alert("Erro ao atualizar o perfil.");
+                                    }
+                                } catch (error) {
+                                    console.error("Erro:", error);
+                                    alert("Erro de conexão com o servidor.");
+                                }
+                                }}
+                            >
+                                <h1 className={styles.h1CN}>Atualizar Perfil</h1>
+
+                                <label className={styles.labelCN}>Nome:</label>
+                                <input
+                                className={styles.input}
+                                type="text"
+                                id="nome"
+                                name="nome"
+                                required
+                                />
+                                <p className={styles.noteCN}>
+                                Esse não é seu nome de usuário nem seu PIN. Esse nome será exibido para seus contatos Concord.
+                                </p>
+
+                                <label className={styles.labelCN}>Descrição:</label>
+                                <input
+                                className={styles.input}
+                                type="text"
+                                id="desc"
+                                name="desc"
+                                required
+                                />
+
+                                <input
+                                type="submit"
+                                className={styles.botaoCN}
+                                value="Atualizar seu perfil"
+                                />
+                            </form>
+                            </section>
                         </div>
+                        </section>
+                    </div>
+                    </div>
                     )}
-                </section>
-            </section>
-        </ProtectedRoute>
+
+
+                {div3 && (
+                    <div className={styles.divSuMUD}>
+                    <div className={styles.containerMUD}>
+                        <div className={styles.headerMUD}></div>
+
+                        <div className={styles.sectionMUD}>
+                        <h3 className={styles.h3MUD}>Avatar atual</h3>
+                        <div className={styles.currentMUD}>
+                            <img src={selectedAvatar} alt="Avatar Atual" />
+                        </div>
+                        </div>
+
+                        <div className={styles.sectionMUD}>
+                        <h3 className={styles.h3MUD}>Alterar avatar</h3>
+                        <div className={styles.gridMUD}>
+                            {avatars.map((avatar, index) => (
+                            <img
+                                width={105}
+                                key={index}
+                                src={avatar}
+                                alt={`avatar ${index + 1}`}
+                                className={`option ${selectedAvatar === avatar ? "selected" : ""}`}
+                                onClick={() => setSelectedAvatar(avatar)}
+                            />
+                            ))}
+                        </div>
+                        </div>
+
+                        <button
+                        className={styles.buttonMUD}
+                        onClick={async () => {
+                            const foto = getFotoNumber();
+
+                            try {
+                            const response = await fetch(
+                                "https://apiconcord.dev.vilhena.ifro.edu.br/updateUser/1-viniciusavila4080@gmail.com-7db940709d83364a8257d6f361b6e13d21622e40",
+                                {
+                                method: "PATCH",
+                                headers: { "Content-Type": "application/json" },
+                                body: JSON.stringify({ foto }),
+                                }
+                            );
+
+                            if (response.ok) {
+                                const data = await response.json();
+                                console.log("Avatar atualizado:", data);
+                                alert("Avatar atualizado com sucesso!");
+                            } else {
+                                alert("Erro ao atualizar avatar.");
+                            }
+                            } catch (error) {
+                            console.error("Erro:", error);
+                            alert("Erro na conexão com o servidor.");
+                            }
+                        }}
+                        >
+                        Atualizar avatar
+                        </button>
+                    </div>
+                    </div>
+                )}
+
+                   
+                    {div5 && (
+                        <button
+                            className={styles.deleteDL}
+                            onClick={async () => {
+                            const confirmar = window.confirm("Tem certeza que deseja apagar sua conta? Essa ação é irreversível.");
+                        
+                            if (confirmar) {
+                            try {
+                                const response = await fetch(
+                                    "https://apiconcord.dev.vilhena.ifro.edu.br/deleteUser/5-vviniciusavila4080@gmail.com-7db940709d83364a8257d6f361b6e13d21622e40",
+                                    {
+                                    method: "DELETE",
+                                    }
+                                );
+                        
+                            if (response.ok) {
+                                alert("Conta apagada com sucesso!");
+                                window.location.href = "/login";
+                                } else {
+                                     alert("Erro ao apagar a conta. Tente novamente.");
+                                }
+                                } catch (error) {
+                                    console.error("Erro:", error);
+                                    alert("Erro ao se conectar ao servidor.");
+                                }
+                                }
+                                }}
+                                >
+                                    Apagar conta
+                                </button>
+                            )}
+                            </section>
+                            </section>
+                            </ProtectedRoute>
     );
 }
 
